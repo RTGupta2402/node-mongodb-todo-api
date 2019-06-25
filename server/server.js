8bf902ab9b02a56d50a89e5e8c6502d9a140e340
GUPTA Aarti <aarti.gupta@soprasteria.com>
2019-06-25 16:11:36 +0530

    Testing GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -46,7 +46,7 @@ app.get('/todos/:id', (req, res) => {
       return res.status(404).send();
     }
 
-    res.send(todo);
+    res.send({todo});
     console.log(JSON.stringify(todo, undefined, 2));
   }, (e) => {
     res.status(404).send();
